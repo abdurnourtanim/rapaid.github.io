f3c17a3b5c556e099f02bab31c1a94ed517d4c79
Abdur Nur Tanim <71883296+antanim@users.noreply.github.com>
2020-12-21 23:14:06 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ window.addEventListener("scroll",function(){
     let navbar = document.querySelector("nav");
     navbar.classList.toggle("sticky", window.scrollY > 255);
 });
+
+AOS.init({
+    offset: 300,
+    duration: 1000
+});
